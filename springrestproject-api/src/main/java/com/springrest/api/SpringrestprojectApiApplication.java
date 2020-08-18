6245291f7c20dd62750fc240c040c3ba8ba0387a
marcelo amorim <marcelo.med.amorim@gmail.com>
2020-08-18 16:10:58 -0300

    Adiciona projeto inicial

---
+++ springrestproject-api/src/main/java/com/springrest/api/SpringrestprojectApiApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestprojectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestprojectApiApplication.class, args);
+	}
+
+}
---
+++ springrestproject-api/src/test/java/com/springrest/api/SpringrestprojectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestprojectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

abb4d5bafa8bc32eed459abd21820f533afbfa08
marcelo amorim <marcelo.med.amorim@gmail.com>
2020-08-20 01:53:22 -0300

    Finaliza aula 2

--- springrestproject-api/src/main/java/com/springrest/osworks/SpringrestprojectApiApplication.java
+++ springrestproject-api/src/main/java/com/springrest/osworks/SpringrestprojectApiApplication.java
@@ -1,4 +1,4 @@
-package com.springrest.api;
+package com.springrest.osworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
